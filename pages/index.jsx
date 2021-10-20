82607e8e645f845ed99b460f41c51a5b7663d79d
sabilimaulana <spacysbl.learn@gmail.com>
2021-10-20 21:01:34 +0700

    Add my profile image

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,48 @@
-import { Container, Box, Heading } from "@chakra-ui/react";
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+} from "@chakra-ui/react";
+// import Image from "next/image";
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I&apos;m a full-stack developer based in Indonesia!
       </Box>
       <Box display={{ md: "flex" }}></Box>
-      <Box flexGrow={1}></Box>
-      <Heading as="h2" variant="page-title">
-        Sabili Maulana
-      </Heading>
-      <p>( Developer )</p>
+      <Box flexGrow={1}>
+        <Heading as="h2" variant="page-title">
+          Sabili Maulana
+        </Heading>
+        <p>( Developer )</p>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/sabili.jpg"
+            alt="Profile Image"
+          />
+        </Box>
+      </Box>
     </Container>
   );
 };
