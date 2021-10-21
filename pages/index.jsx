414d529ea094930d400148451af884310a7b74d8
sabilimaulana <spacysbl.learn@gmail.com>
2021-10-21 20:37:14 +0700

    Add my bio

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,16 @@
 import {
   Container,
+  Button,
   Box,
   Heading,
   Image,
   useColorModeValue,
 } from "@chakra-ui/react";
+import Paragraph from "../components/Paragraph";
 import Section from "../components/Section";
-// import Image from "next/image";
+import NextLink from "next/link";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import { BioSection, BioYear } from "../components/Bio";
 
 const Page = () => {
   return (
@@ -48,7 +52,40 @@ const Page = () => {
         <Heading as="h3" variant="section-title">
           Work
         </Heading>
-        <p>Paragraph</p>
+        <Paragraph>
+          Sabili is a full-stack developer based in Jakarta with a passion for
+          building digital services/stuff he wants. He has a knack for all
+          things launching products, from planning and designing all the way to
+          solving real-life problems with code.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My Portofolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2003</BioYear>
+          Born in Central Java, Indonesia.
+        </BioSection>
+        <BioSection>
+          <BioYear>2021</BioYear>
+          Graduated from SMK Negeri 1 Jakarta
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ❤
+        </Heading>
+        <Paragraph>Music, Playing Chess, Data Structure.</Paragraph>
       </Section>
     </Container>
   );
